808a0ac15d57426be688ec1030116df3959236c3
Simon Kejonen <Simon.m.Kejonen97@gmail.com>
2020-11-09 21:59:42 +0100

    Added classes for all pieces in chess and made it place all pieces in chess acording to the rules.
    Primitive gameloop is in place where white and black take turns moving pieces where only pieces of the right team is allowed.
    
    cleaned up presentation where only the most recent print is shown.

--- chess.py
+++ chess.py
@@ -1,3 +1,5 @@
+import os
+
 class board:
 
     def __init__(self, xSize, ySize):
@@ -27,11 +29,15 @@ class board:
 def make2dList(rows, cols):
     return [ ([0] * cols) for row in range(rows) ]
 
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 class game:
 
     def __init__(self, board):
 
         self.board = board
+        self.currentTeam = "white"
 
 
     def move(self, xOrg, yOrg, xNew, yNew):
@@ -41,13 +47,82 @@ class game:
     
 
     def placeBlack(self):
-        for y in range(0, 2):
-            for x in range(0, self.board.xSize):
-                board.set(self.board, x, y, pawn("black"))
+        for x in range(0, self.board.xSize):                #Places full row of pawns
+            board.set(self.board, x, 1, pawn("black"))
+        
+        board.set(self.board, 0, 0, rook("black"))
+        board.set(self.board, 1, 0, knight("black"))
+        board.set(self.board, 2, 0, bishop("black"))
+        board.set(self.board, 3, 0, queen("black"))
+        board.set(self.board, 4, 0, king("black"))
+        board.set(self.board, 5, 0, bishop("black"))
+        board.set(self.board, 6, 0, knight("black"))
+        board.set(self.board, 7, 0, rook("black"))
+
+
+
     def placeWhite(self):
-        for y in range(self.board.ySize-2, self.board.ySize):
-            for x in range(0, self.board.xSize):
-                board.set(self.board, x, y, pawn("white"))
+        for x in range(0, self.board.xSize):                #Places full row of pawns
+            board.set(self.board, x, self.board.ySize-2, pawn("white"))
+
+        board.set(self.board, 0, 7, rook("white"))
+        board.set(self.board, 1, 7, knight("white"))
+        board.set(self.board, 2, 7, bishop("white"))
+        board.set(self.board, 3, 7, queen("white"))
+        board.set(self.board, 4, 7, king("white"))
+        board.set(self.board, 5, 7, bishop("white"))
+        board.set(self.board, 6, 7, knight("white"))
+        board.set(self.board, 7, 7, rook("white"))
+        
+    def commandInput(self):
+        command = input("option("+self.currentTeam+"): ")
+            
+        if command == "move":
+            posOrg = input("original position(example: 0,1): ")
+            posNew = input("new position(example: 0,3): ")
+
+            if ',' not in posOrg or ',' not in posNew:
+                print("Invalid syntax.")
+                input()
+                return
+
+            oldPos = posOrg.split(',')
+            newPos = posNew.split(',')
+
+            if self.board.get(int(oldPos[0]), int(oldPos[1])).team != self.currentTeam:
+                print("WRONG TEAM SUCKA")
+                input()
+                return
+                
+            try:
+                self.move(int(oldPos[0]), int(oldPos[1]), int(newPos[0]), int(newPos[1]))
+            except IndexError:
+                print("Value is larger than the board.")
+                input()
+            except ValueError:
+                print("Invalid syntax.")
+                input()
+                
+
+        if command == "help":
+            print("move, moves a piece.\nstop, exits the program")
+            input()
+
+        if command == "stop":
+            self.playing = False
+
+
+    def whiteTurn(self):
+        cls()
+        self.currentTeam = "white"
+        self.board.printBoard()
+        self.commandInput()
+        
+    def blackTurn(self):
+        cls()
+        self.currentTeam = "black"
+        self.board.printBoard()
+        self.commandInput()
 
 
     def startGame(self):
@@ -57,35 +132,12 @@ class game:
         self.placeWhite()
 
         while self.playing:
-            self.board.printBoard()
+            if self.playing:
+                self.whiteTurn()
+            if self.playing:
+                self.blackTurn()
 
-            command = input("option(help): ")
             
-
-            if command == "move":
-                posOrg = input("original position(example: 0,1): ")
-                posNew = input("new position(example: 0,3): ")
-
-                if ',' not in posOrg or ',' not in posNew:
-                    print("Invalid syntax.")
-                    continue
-
-                oldPos = posOrg.split(',')
-                newPos = posNew.split(',')
-                
-                try:
-                    self.move(int(oldPos[0]), int(oldPos[1]), int(newPos[0]), int(newPos[1]))
-                except IndexError:
-                    print("Value is larger than the board.")
-                except ValueError:
-                    print("Invalid syntax.")
-
-            if command == "help":
-                print("move, moves a piece.\nstop, exits the program")
-                input()
-
-            if command == "stop":
-                self.playing = False
     
 
 class piece:
@@ -97,10 +149,26 @@ class piece:
         else:
             print(team + " is not a valid team.")
 
+    def isTeam(self, teamName):
+        return self.team == teamName
+
+class king(piece):
+    symbol = 'K'
+
+class queen(piece):
+    symbol = 'Q'
+
+class rook(piece):
+    symbol = 'R'
+
+class bishop(piece):
+    symbol = 'B'
+
+class knight(piece):
+    symbol = 'N'
+
 class pawn(piece):
     symbol = 'P'
-
-
 
 def main():
     b = board(8, 8)
@@ -111,3 +179,21 @@ if __name__ == "__main__":
     main()
 
 
+
+
+#to-do: def whiteTurn at start of game after placing teams. move current inputs to another def, do move method, if piece at first position isTeam("white") do move as usual
+#if !isTeam("white") print("Invalid piece"), at end of def whiteTurn return and then next in startgame is def blackTurn which does the same except with black team.
+#
+#       def startgame:
+#           self.playing = True
+#           self.placeBlack()
+#           self.placeWhite()
+#           while self.playing:
+#               self.whiteTurn()
+#               self.blackTurn()
+#               self.board.printBoard()
+#
+#
+#
+#to-do: change commandInput method to only have start and stop commands and make movementInput method which just does the movement and can do stop as well, call commandInput once before self.playing is set to true
+#at the start of whiteTurn and blackTurn use movementInput call instead of commandInput to bypass initial menu.
